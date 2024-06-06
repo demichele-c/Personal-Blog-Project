88b3ea16e64ae4358103960cc27038b203a57c8c
demichele-c <demichele.charles@yahoo.com>
2024-06-06 12:02:21 -0400

    added light mode and dark mode to blog form page.

--- assets/js/form.js
+++ assets/js/form.js
@@ -5,7 +5,12 @@ inputElements.forEach(input => {
     input.style.boxShadow = '2px 2px 5px rgba(0, 0, 0, 0.1)'; 
 });
 // Added box shadow to all imput boxes.
+const textarea = document.getElementById('content');
 
+// Apply 3D styling to the textarea
+textarea.style.border = '1px solid #ccc';
+textarea.style.boxShadow = '1px 1px 1px #888';
+textarea.style.padding = '5px';
 
 const circleBox = document.getElementById('myCircleBox');
 
@@ -22,3 +27,21 @@ h2.style.fontSize = '40px';
 h2.style.fontFamily = 'Arial, sans-serif'; 
 h2.style.textAlign = 'center';
 // added font style to h2.
+
+const modeToggle = document.getElementById('mode-toggle');
+const body = document.body;
+
+modeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+    updateButtonContent();
+});
+
+function updateButtonContent() {
+    const currentMode = body.classList.contains('dark-mode') ? 'dark' : 'light';
+    modeToggle.textContent = currentMode === 'dark' ? '🌞' : '🌙';
+}
+// added light and dark mode functions.
+const submitButton = document.querySelector('button[type="submit"]');
+
+// Add the 'black-button' class to the submit button
+submitButton.classList.add('black-button');
